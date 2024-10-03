3a67a7951f739b1904f9bf653d5b580093e975f4
varunnn23-eng <1ms21ai061@msrit.edu>
2024-10-03 11:45:51 +0530

    Add files via upload

---
+++ python_test/math_operations.py
@@ -0,0 +1,15 @@
+def sum(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+a=50
+b=60
+print("sum : ", sum(a,b))
+print("difference : ", sub(a,b))
+print("product : ", mul(a,b))
+print("quotient : ", div(a,b))
